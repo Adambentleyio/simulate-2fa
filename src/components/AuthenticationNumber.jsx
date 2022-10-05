507cd555d1ff148e074af266a23eb4d352a0d10e
Adam Bentley <61187053+Adambentleyio@users.noreply.github.com>
2022-10-05 12:59:39 +0100

    typo

--- src/components/AuthenticationNumber.jsx
+++ src/components/AuthenticationNumber.jsx
@@ -30,7 +30,7 @@ const AuthenticationNumber = () => {
     return (
       <div>
         <h2>Enter authentication code</h2>
-        <p>We've text you a unique code to your preffered contact device </p>
+        <p>We've text you a unique code to your preferred contact device </p>
         <p>Enter your code and press enter</p>
         <form className="form__2fa-code" onSubmit={handleAuthenticate}>
             <input type="text" name="n1" maxLength="4" value={inputs} onChange={handleChange}  />
